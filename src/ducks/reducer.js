82da82fce1a8968ebf0e755a6db153a224128a3c
Dan Lubbers <lubbers.dan@gmail.com>
2018-04-09 20:13:09 -0600

    Finished step 11

--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -23,7 +23,18 @@ const UPDATE_PROPERTY_TYPE = 'UPDATE_PROPERTY_TYPE';
 const UPDATE_CITY = 'UPDATE_CITY';
 const UPDATE_PROP = 'UPDATE_PROP';
 const UPDATE_FOUND = 'UPDATE_FOUND';
-
+const UPDATE_AGENT = 'AGENT';
+const UPDATE_COST = 'COST';
+const UPDATE_DOWN_PAYMENT = 'DOWN_PAYMENT';
+const UPDATE_CREDIT = 'CREDIT';
+const UPDATE_HISTORY = 'HISTORY';
+const UPDATE_ADDRESS_1 = 'ADDRESS_1';
+const UPDATE_ADDRESS_2 = 'ADDRESS_2';
+const UPDATE_ADDRESS_3 = 'ADDRESS_3';
+const UPDATE_FIRST_NAME = 'FIRST_NAME';
+const UPDATE_LAST_NAME = 'LAST_NAME';
+const UPDATE_EMAIL = 'EMAIL';
+ 
 // All Action Creator Functions return a 'type' and 'payload'
 export function updateLoanType(loanType) {
     return {
@@ -60,6 +71,84 @@ export function updateFound(found) {
     }
 }
 
+export function updateAgent(agent) {
+    return{
+        type: UPDATE_AGENT,
+        payload: agent
+    }
+}
+
+export function updateCost(cost) {
+    return{
+        type: UPDATE_COST,
+        payload: cost
+    }
+}
+
+export function updateDownPayment(dp) {
+    return{
+        type: UPDATE_DOWN_PAYMENT,
+        payload: dp
+    }
+}
+
+export function updateCredit(credit) {
+    return{
+        type: UPDATE_CREDIT, 
+        payload: credit 
+    }
+}
+
+export function updateHistory(history) {
+    return{
+        type: UPDATE_HISTORY,
+        payload: history 
+    }
+}
+
+export function updateAddress1(address1) {
+    return{
+        type: UPDATE_ADDRESS_1,
+        payload: address1
+    }
+}
+
+export function updateAddress2(address2) {
+    return{
+        type: UPDATE_ADDRESS_2,
+        payload: address2 
+    }
+}
+
+export function updateAddress3(address3) {
+    return{
+        type: UPDATE_ADDRESS_3, 
+        payload: address3
+    }
+}
+
+export function updateFirstName(firstName) {
+    return{
+        type: UPDATE_FIRST_NAME,
+        payload: firstName 
+    }
+}
+
+export function updateLastName(lastName) {
+    return{
+        type: UPDATE_LAST_NAME,
+        payload: lastName 
+    }
+}
+
+export function updateEmail(email) {
+    return{
+        type: UPDATE_EMAIL, 
+        payload: email
+    }
+}
+
+
 function reducer(state = initialState, action){
     switch(action.type) {
          // Adds our action types to our reducer
@@ -74,6 +163,28 @@ function reducer(state = initialState, action){
             return Object.assign( {}, state, {propToBeUsedOn: action.payload} );
         case UPDATE_FOUND:
             return Object.assign( {}, state, {found: action.payload});
+        case UPDATE_AGENT:
+            return Object.assign( {}, state, {agent: action.payload});
+        case UPDATE_COST:
+            return Object.assign( {}, state, {cost: action.payload});
+        case UPDATE_DOWN_PAYMENT:
+            return Object.assign( {}, state, {dp: action.payload});
+        case UPDATE_CREDIT: 
+            return Object.assisgn( {}, state, {cost: action.payload});
+        case UPDATE_HISTORY:
+            return Object.assign( {}, state, {history: action.payload});
+        case UPDATE_ADDRESS_1:
+            return Object.assign( {}, state, {address1: action.payload});
+        case UPDATE_ADDRESS_2:
+            return Object.assign( {}, state, {address2: action.payload});
+        case UPDATE_ADDRESS_3:
+            return Object.assign( {}, state, {address3: action.payload});
+        case UPDATE_FIRST_NAME: 
+            return Object.assign( {}, state, {firstName: action.payload});
+        case UPDATE_LAST_NAME: 
+            return Object.assign( {}, state, {lastName: action.payload});
+        case UPDATE_EMAIL: 
+            return Object.assign( {}, state, {email: action.payload});
 
         // default: if nothing happens, it just returns original state
         default: return state;
